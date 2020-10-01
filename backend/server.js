b679b253c3f4bb9c25e6691faa8df4cfaf2fd519
Jozsef <hollo.jozsef98@gmail.com>
2020-10-01 12:45:08 +0300

    Connected the server to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import data from './data';
+import config from './config';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoutes'
 
+dotenv.config();
+const mongodburl=config.MONGODB_URL;
+mongoose.connect(mongodburl,{
+  useNewUrlParser:true,
+   useUnifiedTopology:true,
+   useCreateIndex:true 
+}).catch(error=>console.log(error.reason));
 const app=express();
+
+app.use("/api/users",userRoute);
 
 app.get("/api/products/:id", (req,res)=>{
     const productId=req.params.id;
