c60e1b4175f8d1c30c664c77fea935161a113114
Jozsef <hollo.jozsef98@gmail.com>
2020-10-05 16:17:46 +0300

    Implemented the edit product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -13,19 +13,36 @@ const listProducts=()=>async (dispatch)=>
     }
 }
 
-const saveProduct=(product)=>async(dispatch,getState)=>{
-try{
-    dispatch({type:PRODUCT_SAVE_REQUEST,payload:product});
-    const {userSignin:{userInfo}}=getState();
-    const{data}=await Axios.post("/api/products/",product,{headers:{
-        'Authorization':'Bearer'+userInfo.token
-    } 
-    });
-    dispatch({type:PRODUCT_SAVE_SUCCESS,payload:data});
-}catch(error){
-    dispatch({type:PRODUCT_SAVE_FAIL ,payload:error.message});
-}
-}
+const saveProduct = (product) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
+      const {
+        userSignin: { userInfo },
+      } = getState();
+      if (!product._id) {
+        const { data } = await Axios.post('/api/products', product, {
+          headers: {
+            Authorization: 'Bearer ' + userInfo.token,
+          },
+        });
+        dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+      } else {
+        const { data } = await Axios.put(
+          '/api/products/' + product._id,
+          product,
+          {
+            headers: {
+              Authorization: 'Bearer ' + userInfo.token,
+            },
+          }
+        );
+        dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+      }
+    } catch (error) {
+      dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
+    }
+  };
+
 
 const detailsProduct = (productId)=>async (dispatch)=>{
     try{
